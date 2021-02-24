ed6206e5a6531fe605d0fa00e1726aea533e4603
Michael Loucks <michael.loucks@codeimmersives.com>
2021-02-24 10:05:57 -0600

    Finished

--- nodeimmersives.js
+++ nodeimmersives.js
@@ -1,2 +1,11 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
 const getInput = require("./get-input.js");
+const firstName = getInput(1);
+const lastName = getInput(2);
+const fullName = firstName +''+lastName;
+const email = firstName.toLowerCase() + '.' + lastName.toLowerCase() + '@codeimmersives.com'
+
+console.log ("hi, " + fullName + "!");
+console.log("your name capatalized is: " + fullName.toUpperCase());
+console.log(" your intials are : " + firstName[0] + '.' +lastName[0] + ".");
+console.log("your Code Immersives email address is: " + email)
